e9805055cd142cff8a2fc20939fe0591399c3b64
Jorge Paiz <jorgepaiz007@gmail.com>
2011-09-24 12:54:33 -0600

    Paiz Foundation Library

---
+++ PaizFoundation/PaizFoundation/PaizFoundation.h
@@ -0,0 +1,13 @@
+//
+//  PaizFoundation.h
+//  PaizFoundation
+//
+//  Created by Eng. Jorge Paiz on 9/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PaizFoundation : NSObject
+
+@end
---
+++ PaizFoundation/PaizFoundation/PaizFoundation.m
@@ -0,0 +1,23 @@
+//
+//  PaizFoundation.m
+//  PaizFoundation
+//
+//  Created by Eng. Jorge Paiz on 9/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PaizFoundation.h"
+
+@implementation PaizFoundation
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
